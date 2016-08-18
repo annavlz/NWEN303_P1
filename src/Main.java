7f76cbb33c5e89a22ad341b62f8d0dc7e90678ae
Anna <anna.v.l@gmail.com>
2016-08-18 20:48:06 +1200

    Main ready

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,32 @@
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 
 public class Main {
-		public static void main(String[] args) {		
-			try {
-				Maze maze = new Maze(new FileReader(args[0]));			
-			} catch(IOException e) {			
-				System.err.println("Error loading file: " + args[0]);
-				System.err.println(e.getMessage());			
-			}
-		}
+	public static void main(String[] args) {
+		String  thisLine = null;
+      try{
+	         BufferedReader br = new BufferedReader(new FileReader(args[0]));
+	         String[] params = br.readLine().split("\\s"); //getParams
+	         int arraySize = Integer.parseInt(params[0]);
+	         int exitX = Integer.parseInt(params[1]);
+	         int exitY = Integer.parseInt(params[2]);
+	         String[][] mazeArray = new String[arraySize][arraySize];
+	         int count = 0;
+	         while ((thisLine = br.readLine()) != null) {
+	        	 String[] row = br.readLine().split("");
+	        	 mazeArray[count] = row;
+	        	 count++;
+	         }       
+	         br.close();
+	         Maze maze = new Maze(mazeArray, exitX, exitY);
+	         maze.findExit(maze, args[1]);
+	         
+	      } catch(Exception e){
+	         e.printStackTrace();
+	      }
+	}
 }
+
+
