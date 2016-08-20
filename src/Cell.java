ed052e179f750a19a61fb3af421c22e6e700e780
Anna <anna.v.l@gmail.com>
2016-08-20 19:49:45 +1200

    Thread work

--- src/Cell.java
+++ src/Cell.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 
 public class Cell {
 	boolean isWall;
@@ -9,13 +11,61 @@ public class Cell {
 	Cell parent;
 	int fNum = 0;
 	boolean isEntry = false;
+	String name;
+//	Collection<Cell> visited = new HashSet<Cell>();
 	
-	public Cell (String type){
-		//System.out.println(type);
-		if(type == "X"){
+	public Cell (String type, String name){
+		if(type.equals("X")){
 			this.isWall = true;
 		} else {
 			this.isWall = false;
 		}
+		this.name = name;
+	}
+	
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Cell))
+			return false;
+		Cell other = (Cell) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+
+
+	@Override
+	public String toString(){
+		String name = "Name: " + this.name;
+		String wall = this.isWall ? " wall," : "";
+		String exitPath = this.exitPath ? " exitPath," : "";
+		String deadEnd = this.deadEnd ? " deadEnd," : "";
+		String entry = this.isEntry ? " entry," : "";
+		int friends = this.fNum;
+//		String parent = "";
+//		if(this.parent.name != null) { 
+//			parent = "parent " + this.parent.name;
+//		} else { }
+		return name + wall + entry + exitPath + deadEnd + "  " + friends + "    ";
 	}
 }
--- src/Maze.java
+++ src/Maze.java
@@ -19,106 +19,32 @@ public class Maze {
 	private void createMaze() {
 		this.entry = mazeArray[entryY][entryX];
 		this.entry.isEntry = true;
-		for(int y = 0; y < mazeArray.length-1; y++){
-			for(int x = 0; x < mazeArray.length-1; x++){
+		for(int y = 0; y < mazeArray.length; y++){
+			for(int x = 0; x < mazeArray.length; x++){
 				Cell cell = mazeArray[y][x];
-				cell.nbrs.add(getCell(mazeArray, y-1,x));
-				cell.nbrs.add(getCell(mazeArray,y+1,x));
-				cell.nbrs.add(getCell(mazeArray,y,x-1));
-				cell.nbrs.add(getCell(mazeArray,y,x+1));
+				addNbr(cell, mazeArray, y-1,x);
+				addNbr(cell, mazeArray,y+1,x);
+				addNbr(cell, mazeArray,y,x-1);
+				addNbr(cell, mazeArray,y,x+1);
+//				System.out.println(cell);
 			}
 		}		
 	}
 
-	private Cell getCell (Cell[][] mazeArray, int x, int y){
+	private void addNbr (Cell me, Cell[][] mazeArray, int y, int x){
 		int size = mazeArray.length;
-		Cell cell;
-		if(x > 0 &&  y > 0 && x < size && y < size){
-			cell = mazeArray[y][x];
-		}else{
-			cell = null;
+		if(x >= 0 &&  y >= 0 && x < size && y < size){
+			me.nbrs.add(mazeArray[y][x]);
 		}
-		return cell;
 	}
 	public void findExit(Maze maze, int fNum) {
 		entry.fNum = fNum;
-		chooseWay(this.entry);	
+		start(this.entry);	
 	}
 	
-	
-	private void chooseWay(Cell from) {
-		ArrayList<Cell> options = findOptions(this.entry);
-		if(options.size() == 1){
-			go(from, options.get(0), from.fNum);
-		} else if (options.size() > 1){
-			divide (from, from.fNum, options);
-		} else {
-			if(from.isEdge && from.fNum == this.entry.fNum){
-				getPath(from);
-				System.out.println("To the pub!");
-			} else{
-				turnBack(from);
-			}
-		}
-	}
-	private void getPath (Cell from) {
-		System.out.println(from.parent);
-		while(from.parent != null){
-			System.out.println(from);
-			getPath(from.parent);
-		}
-	}
-	
-	private void turnBack(Cell from) {
-		ActionB _return = new ActionB (from); 
-		Thread goBack = new Thread(_return);
-		goBack.start();
-		//move.join();
-		if(_return.canProceed){
-			turnBack(from.parent);
-		}else{
-			ArrayList<Cell>options = findOptions(from);
-			if(options.size() > 1){
-				chooseWay(from);
-			}
-		}
-		
-	}
-
-	private void divide (Cell from, int group, ArrayList<Cell>options){
-		if (group > 0){
-			int party = group / options.size();
-			if(party == 0){
-				party = 1;
-			}
-			go(from, options.get(0), party);
-			options.remove(0);
-			divide(from, group - party, options);
-		}
-	}
-	
-	public void go (Cell from, Cell to, int howMany) {
-		ActionF action = new ActionF (from, to, howMany); 
+	public void start (Cell from) {
+		Action action = new Action(from); 
 		Thread move = new Thread(action);
 		move.start();
-		//move.join();
-		if(action.canProceed){
-			chooseWay(to);
-		} 
-	}
-	
-	public static ArrayList<Cell> findOptions (Cell point){
-		ArrayList<Cell> options = new ArrayList<Cell>();
-			for(Cell nbr : point.nbrs){
-				if(nbr.exitPath == true){
-					ArrayList<Cell>exitPath = new ArrayList<Cell>();
-					exitPath.add(nbr);
-					return exitPath;
-				}
-				if(!(nbr == null) && !nbr.isWall && !nbr.deadEnd){
-					options.add(nbr);
-				}
-			}
-		return options;
 	}
 }
